a2f58835c0a7c018780f7d3a5f622b15a615894b
Marcelo <mmunizdaluz@gmail.com>
2020-08-30 17:06:41 -0300

    Salvando em localStorege

--- src/App.js
+++ src/App.js
@@ -1,16 +1,85 @@
 import React, { useMemo, useState, useCallback } from "react";
-import { createEditor, Transforms, Editor, Text } from 'slate';
+import { createEditor, Transforms, Editor, Text, Node } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 
+const serialize = value =>{
+  return (value.map(n => Node.string(n)).join('\n'));
+};
+
+const deserialize = string => {
+  return string
+    .split('\n')
+    .map(line => {
+      return { children: [{ text: line }], }
+    });
+};
+
+const Leaf = props => {
+  return(
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal'}}
+    >
+      {props.children}
+    </span>
+  )
+}
+
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>
+        { props.children }
+      </code>
+    </pre>
+  );
+};
+
+const DefaultElement = props => {
+  return <p {...props.attributes}> { props.children }</p>
+};
+
+const CustomEditor = {
+  isBoldMarkActive(editor){
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal:true,
+    })
+
+    return !!match
+  },
+
+  isCodeBlockActive(editor){
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === 'code',
+    });
+    return !!match;
+  },
+
+  toggleBoldMark(editor){
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor, 
+      { bold: isActive ? null : true},
+      {match: n => Text.isText(n), split: true}
+    )
+  },
+
+  toggleCodeBlock(editor){
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor, 
+      { type: isActive ? null : 'code'},
+      { match: n => Editor.isBlock(editor, n)}
+    )
+  }
+}
 
 function App() {
   const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ]);
+  //deserialize(localStorage.getItem('content')) ||
+  const [value, setValue] = useState(
+    deserialize(localStorage.getItem('content')) || '');
 
   const renderElement = useCallback(props => {
     switch(props.element.type){
@@ -29,7 +98,30 @@ function App() {
     <Slate 
       editor= {editor}
       value={value} 
-      onChange={newValue => setValue(newValue) } >
+      onChange={newValue => { 
+          setValue(newValue);
+          localStorage.setItem('content', serialize(newValue))
+
+        }
+      }>
+        <div>
+          <button
+            onMouseDown={event => {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+            }}
+          >
+            Bold
+          </button>
+          <button
+            onMouseDown={event => {
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+            }}
+          >
+            Code Block
+          </button>
+        </div>
         <Editable 
           renderElement={renderElement}
           renderLeaf={renderLeaf}
@@ -38,32 +130,14 @@ function App() {
             switch(event.key){
               case 'c':{
                 event.preventDefault();
-
-                const [match] = Editor.nodes(editor, {
-                  match: n => n.type === 'code',
-                });
-
-                Transforms.setNodes(
-                  editor,
-                  { type: match ? 'paragraph' : 'code' },
-                  { match: n => Editor.isBlock(editor, n) }
-                );
+                CustomEditor.toggleCodeBlock(editor);
                 break;
               }
-              
               case 'b':{
-                event.preventDefault();
-                
-                Transforms.setNodes(
-                  editor, 
-                  { bold: true }, 
-                  { match: n => Text.isText(n), split: true}
-                )
-                
-                break
+                event.preventDefault();                
+                CustomEditor.toggleBoldMark(editor); 
+                break;
               }
-                
-
               default:
               
             }
@@ -73,28 +147,6 @@ function App() {
   );
 }
 
-const Leaf = props => {
-  return(
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal'}}
-    >
-      {props.children}
-    </span>
-  )
-}
-const CodeElement = props => {
-  return (
-    <pre {...props.attributes}>
-      <code>
-        { props.children }
-      </code>
-    </pre>
-  );
-};
 
-const DefaultElement = props => {
-  return <p {...props.attributes}> { props.children }</p>
-};
 
 export default App;
